572b18c0e20a97a263e81445c12f5c2d88db0317
LinSrHang <1023948565@qq.com>
2025-02-07 03:21:26 +0800

    为stl.transform提供slice和map的FilterXXX方法

--- stl/transform/map.go
+++ stl/transform/map.go
@@ -19,3 +19,27 @@ func MapFromMap[SRC_K comparable, SRC_V any, DST_K comparable, DST_V any](src ma
 	}
 	return ret
 }
+
+// FilterMapFromSlice 按 fn 所定义的步骤将 SRC 类型切片转换为 map[K]V 类型字典，仅当 bool 为 true 时才将值插入 map[K]V
+func FilterMapFromSlice[SRC any, K comparable, V any](src []SRC, fn func(int, SRC) (K, V, bool)) map[K]V {
+	ret := make(map[K]V, len(src))
+	for idx, val := range src {
+		k, v, ok := fn(idx, val)
+		if ok {
+			ret[k] = v
+		}
+	}
+	return ret
+}
+
+// FilterMapFromMap 按 fn 所定义的步骤将 SRC_K, SRC_V 类型字典转换为 DST_K, DST_V 类型字典，仅当 bool 为 true 时才将值插入 map[DST_K]V
+func FilterMapFromMap[SRC_K comparable, SRC_V any, DST_K comparable, DST_V any](src map[SRC_K]SRC_V, fn func(SRC_K, SRC_V) (DST_K, DST_V, bool)) map[DST_K]DST_V {
+	ret := make(map[DST_K]DST_V, len(src))
+	for k, v := range src {
+		dk, dv, ok := fn(k, v)
+		if ok {
+			ret[dk] = dv
+		}
+	}
+	return ret
+}
--- stl/transform/slice.go
+++ stl/transform/slice.go
@@ -17,3 +17,27 @@ func SliceFromMap[K comparable, V any, DST any](src map[K]V, fn func(K, V) DST) 
 	}
 	return ret
 }
+
+// FilterSliceFromSlice 将 SRC 类型切片按 fn 所定义的步骤转换为 DST 类型切片，仅当 bool 为 true 时才将值插入 []DST
+func FilterSliceFromSlice[SRC any, DST any](src []SRC, fn func(int, SRC) (DST, bool)) []DST {
+	ret := make([]DST, 0, len(src))
+	for idx, val := range src {
+		newVal, ok := fn(idx, val)
+		if ok {
+			ret = append(ret, newVal)
+		}
+	}
+	return ret
+}
+
+// FilterSliceFromMap 将 map[K]V 类型字典按 fn 所定义的步骤转换为 DST 类型切片，仅当 bool 为 true 时才将值插入 []DST
+func FilterSliceFromMap[K comparable, V any, DST any](src map[K]V, fn func(K, V) (DST, bool)) []DST {
+	ret := make([]DST, 0, len(src))
+	for k, v := range src {
+		newVal, ok := fn(k, v)
+		if ok {
+			ret = append(ret, newVal)
+		}
+	}
+	return ret
+}
